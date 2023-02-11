42dff6f9f19470a83622125c60754fd83ef2096b
Chris Gorham <chris.gorham@outlook.com>
2023-02-11 11:42:50 -0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -23,10 +23,11 @@ class Footer extends HTMLElement
                 <a href="https://github.com/chrisgorham999" target="resource window">Chris Gorham GitHub Repositories</A><br>
                 
                 <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="resource window">Bellevue University Web Development Degree</A><br>
+                <a href="https://www.youtube.com/user/bellevueuniversity" target="resource window">Bellevue University YouTube Channel</A><br>
                 <a href="https://github.com/buwebdev/web-330" target="resource window">Bellevue Web 330 GitHub</A><br>
                 </p>
                 <p class="footer-links">
-                Copyright &copy; 2023 <a href="#">Bellevue University</a>
+                Copyright &copy; 2023 <a href="https://www.bellevue.edu target="resource window">Bellevue University</a>
                 </p>
                 </footer>
         `;
