082d688db1611696b3ef468cb8a04c10dda2e04e
Chris Gorham <chris.gorham@outlook.com>
2023-02-19 19:19:00 -0500

    sprint 2 updates

--- header.js
+++ header.js
@@ -20,41 +20,41 @@ class Header extends HTMLElement
     connectedCallback() {
         this.innerHTML = `
         <!-- Icon Bar (Sidebar - hidden on small screens) -->
-        <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+        <nav class="w3-sidebar w3-bar-block gorham-small w3-hide-small w3-center">
           <!-- Avatar image in top left corner -->
           <img class="gorham-filter gorham-opacity gorham-opacity-off" src="images/portrait.png" style="width:100%">
-          <a href="index.html" class="w3-bar-item w3-button w3-padding-large w3-black">
-            <i class="fa fa-home w3-xxlarge"></i>
+          <a href="index.html" class="w3-bar-item w3-button gorham-padding-12 gorham-black">
+            <i class="fa fa-home gorham-xxlarge"></i>
             <p>HOME</p>
           </a>
-          <a href="index.html#about" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-            <i class="fa fa-user w3-xxlarge"></i>
+          <a href="index.html#about" class="w3-bar-item w3-button gorham-padding-12 w3-hover-black">
+            <i class="fa fa-user gorham-xxlarge"></i>
             <p>ABOUT</p>
           </a>
-          <a href="resume.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-          <i class="fa fa-solid fa-file w3-xxlarge"></i>
+          <a href="resume.html" class="w3-bar-item w3-button gorham-padding-12 w3-hover-black">
+          <i class="fa fa-solid fa-file gorham-xxlarge"></i>
           <p>RESUME</p>
           </a>
-          <a href="projects.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-            <i class="fa fa-solid fa-folder w3-xxlarge"></i>
+          <a href="projects.html" class="w3-bar-item w3-button gorham-padding-12 w3-hover-black">
+            <i class="fa fa-solid fa-folder gorham-xxlarge"></i>
             <p>PROJECTS</p>
           </a>
-          <a href="database-diagrams.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-          <i class="fa fa-solid fa-database w3-xxlarge"></i>
+          <a href="database-diagrams.html" class="w3-bar-item w3-button gorham-padding-12 w3-hover-black">
+          <i class="fa fa-solid fa-database gorham-xxlarge"></i>
           <p>DATABASE DIAGRAMS</p>
           </a>
-          <a href="api.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-          <i class="fa fa-laptop w3-xxlarge"></i>
+          <a href="api.html" class="w3-bar-item w3-button gorham-padding-12 w3-hover-black">
+          <i class="fa fa-laptop gorham-xxlarge"></i>
           <p>API UNIT TESTS</p>
           </a>
-          <a href="index.html#bucket-list" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-          <i class="fa fa-list w3-xxlarge"></i>
+          <a href="index.html#bucket-list" class="w3-bar-item w3-button gorham-padding-12 w3-hover-black">
+          <i class="fa fa-list gorham-xxlarge"></i>
           <p>BUCKET LIST</p>
           </a>
           </nav>
         
 
-        <div class="w3-padding-large" id="main">
+        <div class="gorham-padding-12" id="main">
         <!-- Header/Home -->
         <header class="gorham-logo" id="home">
           <img class="gorham-filter" src="images/logo.png"><br><br><br>
